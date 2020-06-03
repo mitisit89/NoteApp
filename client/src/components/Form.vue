8b31c28bf1a16eec808d9434d81b24066b455f49
Wladislaw <mitisit89@gmail.com>
2020-06-03 23:32:04 +0300

    форма

---
+++ client/src/components/Form.vue
@@ -0,0 +1,39 @@
+<template>
+	<form @submit.prevent="onSubmint">
+		<input type="text" v-model="title" />
+		<button type="submit">Add</button>
+	</form>
+</template>
+<script>
+	export default {
+		data() {
+			return {
+				title: ""
+			};
+		},
+		methods: {
+			onSubmint() {
+				console.log("on", this.title);
+				if (this.title.trim()) {
+					const newObjectItem = {
+                        id: Date.now(),
+                        title:this.title
+                    };
+                    this.$emit('add-item', newObjectItem )
+				}
+			}
+		}
+	};
+</script>
+
+<style  scoped>
+form {
+	display: flex;
+}
+input {
+	width: 400px;
+}
+button {
+	margin-left: 1px;
+}
+</style>
