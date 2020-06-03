9c30be88a494c5bf9ec2e8d817b836bd2b495dcf
Wladislaw <mitisit89@gmail.com>
2020-06-03 23:34:08 +0300

    прогресс

--- client/src/components/RecipeCard.vue
+++ client/src/components/RecipeCard.vue
@@ -1,24 +1,31 @@
-<template >
+<template>
 	<div class="card">
-		<h1>{{item.id}}</h1>
-		<div class="card-body">{{item.title }}</div>
+		<h1>{{ item.id }}</h1>
+		<div class="card-body">{{ item.title }}</div>
 		<span class="datetime">datetime</span>
 		<span class="tags">tags</span>
+		<button class="rm-btn" v-on:click="$emit('remove-item', item.id)">
+			&times;
+		</button>
 	</div>
 </template>
 <script>
-export default {
-	props:{
-		item:{
-			type:Object,
-			require:true
+	export default {
+		props: {
+			item: {
+				type: Object,
+				require: true
+			}
 		}
-	}
-}
+	};
 </script>
 <style scoped>
-.card{
-	border: 1px solid black;
-	padding: 1%;
-}
+	.card {
+		border: 1px solid black;
+		padding: 1%;
+		margin-top: 2%;
+	}
+	.rm-bnt {
+		border-radius: 50%;
+	}
 </style>
