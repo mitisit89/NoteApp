d30568e5f980645fea4af90a88443d87b8bdbf59
Wladislaw <mitisit89@gmail.com>
2020-06-07 21:19:00 +0300

    added vue -router

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,56 +1,8 @@
 <template>
 	<div id="app">
-		<Index v-bind:objects="objects" @remove-item="RemoveItem" @add-item='AddItem'/>
+	<router-view />
 	</div>
 </template>
 
-<script>
-	import Index from "@/components/Index"; 
 
-	export default {
-		name: "App",
-		data() {
-			return {
-				objects: [
-					{ id: 1, title: "lorem ipsum loeeodedddfd" },
-					{ id: 2, title: "teew" },
-					{ id: 3, title: "23ds" }
-				]
-			};
-		},
-		methods: {
-			RemoveItem(id) {
-				this.objects = this.objects.filter(t => t.id !== id);
-			},
-			AddItem(item){
-				this.objects.push(item)
-			}
-		},
-		components: {
-			Index
-		}
-	};
-</script>
 
-<style>
-@import url("https://fonts.googleapis.com/css?family=Ubuntu&display=swap&subset=cyrillic-ext");
-
-* {
-	box-sizing: border-box;
-}
-
-html,
-body {
-	font-family: "Ubuntu", sans-serif;
-	background-color: #f1f5ff;
-	margin: 0;
-}
-
-a {
-	text-decoration: none;
-}
-
-li {
-	list-style-type: none;
-}
-</style>
