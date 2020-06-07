063d47d6f40005ed1e415e644d0af5a450c2e90e
Wladislaw <mitisit89@gmail.com>
2020-06-07 21:17:00 +0300

    add vue  router

---
+++ client/src/views/Home.vue
@@ -0,0 +1,6 @@
+<template>
+<div>
+	<h2>Home page</h2>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente perferendis aperiam eveniet, rerum magni cum inventore fugiat nulla maiores incidunt asperiores doloremque quasi, provident vero dolorum delectus accusamus dolor officia!</p>
+    <router-link to="/notes"> list</router-link></div>
+</template>
