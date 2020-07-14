ba109c26b165243273c802a3cd939fcf76690852
Wladislaw <mitisit89@gmail.com>
2020-07-14 20:11:45 +0300

    Update routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask, send_file
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+#from flask_migrate import Migrate
 from config import Configuration
 
-app = Flask(__name__, static_folder='client/dist', static_url_path='')
+app = Flask(__name__, static_folder='../client/dist', static_url_path='')
 app.config.from_object(Configuration)
 db = SQLAlchemy(app)
 
-migrate = Migrate(app, db)
+#migrate = Migrate(app, db)
 from app.models import User, Recipe
 
 from app.routes import rest
@@ -17,4 +17,4 @@ app.register_blueprint(rest)
 
 @app.route('/')
 def index():
-    return send_file('client/dist/index.html')
+    return send_file('../client/dist/index.html')
