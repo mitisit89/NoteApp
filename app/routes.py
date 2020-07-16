da75d831e31c20831d2124f1800ee7f5886495e3
Wladislaw <mitisit89@gmail.com>
2020-07-16 20:08:54 +0300

    отказ от  flask resful

--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,21 @@
+
+from flask import Blueprint,jsonify
 from app import app
 from app.models import Recipe
-from flask import Blueprint
-from flask_restful import Api, reqparse, request, Resource
-rest=Blueprint('rest',__name__)
-api = Api(app)
+
+rest = Blueprint('rest', __name__)
 
 
-class HelloWorld(Resource):
-    def get(self):
-        posts = Recipe.query.all()
-        data = []
-        for post in posts:
-            i = {'id': str(post.id), 'title': post.title, 'slug': post.slug, 'body': post.body, 'time': str(post.time_stamp)}
-            data.append(i)
-        return  data
-    def post(self):
-            pass
-api.add_resource(HelloWorld,  '/api/hello')
+@app.route('/api/', methods=['GET'])
+def get():
+    posts = Recipe.query.all()
+    data =[]
+    for post in posts:
+        i = {'id': str(post.id), 'title': post.title, 'slug': post.slug, 'body': post.body,
+             'time': str(post.time_stamp)}
+        data.append(i)
+    return jsonify(data)
+
+
+def post():
+    pass
