9c8a4fae795f7211dc2b75965fd4e7cb45fca7e6
Wladislaw <mitisit89@gmail.com>
2020-07-29 17:09:21 +0300

    зароботало

--- client/src/components/RecipeCard.vue
+++ client/src/components/RecipeCard.vue
@@ -1,45 +1,47 @@
 <template>
-	<div class="card">
-		<h2>{{ item.id }}</h2>
-		<div class="card-body">{{ item.title }}</div>
-		<div class="meta"><span class="datetime">datetime</span>
-		<span class="tags">tags</span></div>
-		
-		<button class="rm-btn" v-on:click="$emit('remove-item', item.id)">
-			&times;
-		</button>
-	</div>
+  <div class="card">
+    <h2>{{ todo.id }}</h2>
+    <div class="card-body">{{ todo.title }}</div>
+    <div class="meta"><span class="datetime">datetime</span>
+      <span class="tags">tags</span></div>
+      <div > {{todo}}</div>
+    <button class="rm-btn" v-on:click="$emit('remove-item', todo.id)">
+      &times;
+    </button>
+  </div>
 </template>
 <script>
-	export default {
-		props: {
-			item: {
-				type: Object,
-				require: true
-			}
-		}
-	};
+export default {
+  props: {
+    todo: {
+      type: Object,
+      require: true
+    }
+  }
+};
 </script>
 <style scoped>
-	.card {
-		display: inline-flex;
-		flex-direction: column;
-		justify-content:  space-between;
-		border: 1px solid black;
-		padding: 1.25rem;
-		margin-top: 2%;
-		
-	}
-	.rm-btn {
-		border-radius: 40%;
-		padding: 1rem;
-		width: 10px;
-		top: 0;
-		right: 0;
-		position: static;
+.card {
+  display: inline-flex;
+  flex-direction: column;
+  justify-content: space-between;
+  border: 1px solid black;
+  padding: 1.25rem;
+  margin-top: 2%;
 
-	}
-	h2{
-		text-decoration-line: underline;
-	}
+}
+
+.rm-btn {
+  border-radius: 40%;
+  padding: 1rem;
+  width: 10px;
+  top: 0;
+  right: 0;
+  position: static;
+
+}
+
+h2 {
+  text-decoration-line: underline;
+}
 </style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,65 +1,72 @@
 <template>
-	<div class="conteiner">
-		
-
-		<h1>The Book of Recipe</h1>
-		<Form @add-item='AddItem'/>
-		<div class="row">
-			<!-- eslint-disable-next-line-->
-			<RecipeCard
-				v-for="item in objects"
-				v-bind:item="item"
-				v-on:remove-item="RemoveItem"
-			/>
-		</div>
-	</div>
+  <div class="container">
+    <h1>The Book of Recipe</h1>
+    <Form @add-item="AddItem"/>
+    <div class="row">
+      <RecipeCard v-for="todo of todos" v-bind:todo="todo" :key="todo.id" v-on:remove-item="RemoveItem"/>
+    </div>
+  </div>
 </template>
 
 <script>
-	import RecipeCard from "@/components/RecipeCard";
-	import Form from "@/components/Form";
-	export default {
-		props: ["objects"],
-		components: {
-			RecipeCard,
-			Form
-		},
-		methods: {
-			RemoveItem(id) {
-				console.log(id);
-				this.$emit("remove-item", id);
-			},
-			AddItem(item){
-				console.log(item);
-				this.$emit("add-item",item)
-			},
-		}
-	};
+import RecipeCard from "@/components/RecipeCard";
+import Form from "@/components/Form";
+
+export default {
+  components: {
+    RecipeCard,
+    Form,
+  },
+  data() {
+    return {
+      todos: [
+
+      ]
+    }
+  },
+  mounted() {
+    fetch('https://jsonplaceholder.typicode.com/todos/')
+        .then(response => response.json())
+        .then(json =>{this.todos=json})
+  },
+  methods: {
+    RemoveItem(id) {
+      console.log(id);
+      this.$emit("remove-item", id);
+    },
+    AddItem(item) {
+      console.log(item);
+      this.$emit("add-item", item);
+    },
+  },
+};
 </script>
 
 <style scoped>
-	.container {
-		display: flex;
-		flex: 1;
-		justify-content: space-around;
-		position: relative;
-		flex-direction: column;
-		padding: 10px;
-		margin: 10px;
-		background-color: #8fabf2;
-	}
-	h1 {
-		text-decoration: underline;
-	}
-	.row {
-		display: flex;
-		justify-content: space-around;
-		position: relative;
-		flex-direction: inherit;
-		padding: 0.5rem 2rem;
-		margin-bottom: 1.5rem;
-		align-content: stretch;
-		align-items: baseline;
-		flex-wrap: wrap;
-	}
+.container {
+  display: flex;
+  flex: 1;
+  justify-content: space-around;
+  position: relative;
+  flex-direction: column;
+  padding: 10px;
+  margin: 10px;
+  background-color: #8fabf2;
+}
+
+h1 {
+  text-decoration: underline;
+}
+
+.row {
+  display: flex;
+  justify-content: space-around;
+  position: relative;
+  flex-direction: inherit;
+  padding: 0.5rem 2rem;
+  margin-bottom: 1.5rem;
+  align-content: stretch;
+  align-items: baseline;
+  flex-wrap: wrap;
+}
 </style>
