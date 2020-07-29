ee9aed2cd04d569a803c1b8a4c43f06af10f1ad0
Wladislaw <mitisit89@gmail.com>
2020-07-29 17:13:40 +0300

    Отключение сортировки JSON

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ class Configuration(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                              'sqlite:///' + os.path.join(basedir, 'app.db')
-    JSON_SORT_KEYS=False
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    JSON_SORT_KEYS = False
