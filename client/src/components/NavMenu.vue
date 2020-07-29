bdfc512422ff2bde2d90d8e6cc1937f7b0dacb0b
Wladislaw <mitisit89@gmail.com>
2020-07-29 17:08:52 +0300

    форматирование

--- client/src/components/NavMenu.vue
+++ client/src/components/NavMenu.vue
@@ -1,69 +1,72 @@
 <template>
-	<nav class="navigation">
-		<div class="nav-menu">
-			<ul class="nav-list">
-				<li class="nav-link">
-					<router-link to='/'>Главная</router-link>
-				</li>
+  <nav class="navigation">
+    <div class="nav-menu">
+      <ul class="nav-list">
+        <li class="nav-link">
+          <router-link to='/'>Главная</router-link>
+        </li>
 
-				<li class="nav-link">
-					<a href="#">Lorem3</a>
-				</li>
+        <li class="nav-link">
+          <router-link to='/page2'>Page2</router-link>
+        </li>
 
-				<li class="nav-link">
-					<a href="#">Lorem4</a>
-				</li>
-				<li class="nav-link">
-					<a href="#">Admin</a>
-				</li>
+        <li class="nav-link">
+          <a href="#">Lorem4</a>
+        </li>
+        <li class="nav-link">
+          <a href="#">Admin</a>
+        </li>
 
-				<li class="nav-link">
-					<a href="#">Создать запись</a>
-				</li>
-				<li class="nav-link">
-					<a href="#">Выход</a>
-				</li>
-				<li class="nav-link">
-					<a href="#">Вход</a>
-				</li>
-				<li class="nav-link">
-					<a href="#">Регистрация</a>
-				</li>
-			</ul>
-		</div>
-	</nav>
+        <li class="nav-link">
+          <a href="#">Создать запись</a>
+        </li>
+        <li class="nav-link">
+          <a href="#">Выход</a>
+        </li>
+        <li class="nav-link">
+          <a href="#">Вход</a>
+        </li>
+        <li class="nav-link">
+          <a href="#">Регистрация</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
-	export default {};
+export default {};
 </script>
 
 <style>
 .navigation {
-	display: flex;
-	flex-flow: row;
-	background-color: #3d3d3f;
-	color: white;
-	padding: 10px;
-	margin: 10px;
-	position: relative;
-	align-content: space-between;
+  display: flex;
+  flex-flow: row;
+  background-color: #3d3d3f;
+  color: white;
+  padding: 10px;
+  margin: 10px;
+  position: relative;
+  align-content: space-between;
 }
+
 .nav-list,
 .auth-list {
-	display: inline-flex;
+  display: inline-flex;
 }
 
 .nav-link {
-	flex-flow: row wrap;
-	padding: inherit;
+  flex-flow: row wrap;
+  padding: inherit;
 }
+
 .nav-link a {
-	color: white;
+  color: white;
 }
-.nav-menu{
-display:flex;
-align-content: inherit;
+
+.nav-menu {
+  display: flex;
+  align-content: inherit;
   align-content: space-between;
 }
 
--- client/src/router.js
+++ client/src/router.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/Home'
+import Page2 from '@/views/Page2'
 
 Vue.use(Router)
 
 export default new Router({
-    mode:'history',
-    routes:[
+    mode: 'history',
+    routes: [
         {
-            path:'/',
-            component:Home
+            path: '/',
+            component: Home
+        },
+        {
+            path: '/page2',
+            component: Page2
         }
     ]
 })
