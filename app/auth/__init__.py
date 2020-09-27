a26de4eef4585e1b83a4eb0fb2d8c1b1846507ea
Vladislav <mitisit89@gmail.com>
2020-09-27 16:20:50 +0300

    регистрация работает

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,22 +1,24 @@
-from flask import Blueprint, request,make_response
+from flask import Blueprint, request, make_response
 from app import User, db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_cors import CORS
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('api/login', methods=['GET'])
+@auth.route('/api/auth/login', methods=['GET'])
 def check_current_user():
     current_user = {
         'email': request.json['email'],
         'password': request.json['password']
     }
     user = User.query.filter_by(email=current_user['email']).first()
-    if user is None or check_password_hash(user.password, current_user['password']):
-        return '404'
+    if user is None or not check_password_hash(user.password, current_user['password']):
+        return '400'
+    return '200'
 
 
-@auth.route('api/registation', methods=['POST'])
+@auth.route('/api/auth/registration', methods=['POST'])
 def create_new_user():
     new_user = {
         'username': request.json['username'],
@@ -31,5 +33,6 @@ def create_new_user():
         add_new_user = User(username=user_name, email=email, password=password)
         db.session.add(add_new_user)
         db.session.commit()
+        return '200'
     else:
-        return ''
+        return '404'
