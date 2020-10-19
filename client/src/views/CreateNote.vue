bd3dc91d088f838420be8a042a6c2f927f2167b2
Vladislav <mitisit89@gmail.com>
2020-10-19 16:59:20 +0300

    Страница для создания заметок

---
+++ client/src/views/CreateNote.vue
@@ -0,0 +1,28 @@
+<template>
+  <Form @add-item="AddItem" />
+</template>
+<script>
+import Form from "@/components/Form";
+export default {
+  components: {
+    Form,
+  },
+  methods: {
+    AddItem(item) {
+      console.log(item);
+      this.$emit("add-item", item);
+      fetch("http://127.0.0.1:5000/api/postData", {
+        method: "POST",
+        headers: { "Content-Type": "application/json;charset=utf-8" },
+        body: JSON.stringify(item),
+      }).then((response)=>{
+         if (response.ok){
+             this.$router.push('/')
+         }
+      }).catch((e)=>{
+          console.error(e);
+      })
+    },
+  },
+};
+</script>
