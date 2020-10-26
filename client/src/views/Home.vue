abd402a5bc8dbfd52e7cc1b7d0209e81d40e6f02
Vladislav <mitisit89@gmail.com>
2020-10-26 20:56:08 +0300

    Форматироване

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,7 +2,12 @@
   <div class="container">
     <h1>The Book of Recipe</h1>
     <div class="row">
-      <RecipeCard v-for="todo of todos" v-bind:todo="todo" :key="todo.id" v-on:remove-item="RemoveItem"/>
+      <RecipeCard
+        v-for="todo of todos"
+        v-bind:todo="todo"
+        :key="todo.id"
+        v-on:remove-item="RemoveItem"
+      />
     </div>
   </div>
 </template>
@@ -15,32 +20,30 @@ export default {
   },
   data() {
     return {
-      todos: [
-
-      ]
-    }
+      todos: [],
+    };
   },
   mounted() {
-    fetch('http://127.0.0.1:5000/api/getData',{
-      headers:{
-        'Content-Type': 'application/json;charset=utf-8',
-        'Allow':'http://10.0.1.9:8080/',
-        
-      }
+    fetch("http://127.0.0.1:5000/api/getData", {
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+        Allow: "http://10.0.1.9:8080/",
+      },
     })
-        .then(response => response.json())
-        .then(json =>{this.todos=json})
+      .then((response) => response.json())
+      .then((json) => {
+        this.todos = json;
+      });
   },
   methods: {
     RemoveItem(id) {
       console.log(id);
-    this.todos=this.todos.filter(t => t.id !==id)
+      this.todos = this.todos.filter((t) => t.id !== id);
 
-      fetch(`http://127.0.0.1:5000/api/delData/${id}`,{
-        method:'DELETE'
-      })
-      },
-    
+      fetch(`http://127.0.0.1:5000/api/delData/${id}`, {
+        method: "DELETE",
+      });
+    },
   },
 };
 </script>
@@ -49,11 +52,14 @@ export default {
 .container {
   display: flex;
   flex: 1;
+   max-width: 1440px;
+  margin: 0px auto;
+  padding: 0px 15px;
+  z-index: 1;
+  padding-top: 1rem;
   justify-content: space-around;
   position: relative;
   flex-direction: column;
-  padding: 10px;
-  margin: 10px;
   background-color: #8fabf2;
 }
 
@@ -65,11 +71,26 @@ h1 {
   display: flex;
   justify-content: space-around;
   position: relative;
-  flex-direction:initial;
+  flex-direction: initial;
   padding: 0.5rem 2rem;
   margin-bottom: 1.5rem;
   align-content: stretch;
   align-items: baseline;
   flex-wrap: wrap;
+}
+@media (max-width: 1400px) {
+  .container , #navigaton {
+    max-width: 970px;
+  }
+}
+@media (max-width: 992px) {
+  .container,#navigaton {
+    max-width: 750px;
+  }
+}
+@media (max-width: 767px) {
+  .container,#navigaton {
+    max-width: none;
+  }
 }
 </style>
