3eca494eb01c912b504c90b953f5e7eb77c81d16
Vladislav <mitisit89@gmail.com>
2020-11-30 16:44:02 +0300

    модуль Authsotrage

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import PostStorage from './modules/PostStorage'
+import AuthStorage  from './modules/AuthStorage'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     modules: {
-        PostStorage
+        PostStorage,
+        AuthStorage 
     }
 })
