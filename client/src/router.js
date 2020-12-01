aa32860ae73332b31a3d06d0e87486de04bb1a1d
Vladislav <mitisit89@gmail.com>
2020-12-01 22:21:18 +0300

    проверка на авторизацию

--- client/src/router.js
+++ client/src/router.js
@@ -1,35 +1,55 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from '@/views/Home'
-import Page2 from '@/views/Page2'
-import Login from '@/views/Login'
-import CreateNote from '@/views/CreateNote'
-import Registration from '@/views/Registration'
-
-Vue.use(Router)
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "@/views/Home";
+import Page2 from "@/views/Page2";
+import Login from "@/views/Login";
+import CreateNote from "@/views/CreateNote";
+import Registration from "@/views/Registration";
+import store from "@/store";
+Vue.use(Router);
+//переделать 
+const ifNotLogin = (to, from, next) => {
+  if (!store.getters.getIslogin) {
+    next();
+    return;
+  }
+  next("/");
+};
+const ifLogin = (to, from, next) => {
+  if (store.getters.getIslogin) {
+    next();
+    return;
+  }
+  next("/login");
+};
 
 export default new Router({
-    mode: 'history',
-    routes: [
-        {
-            path: '/',
-            component: Home
-        },
-        {
-            path: '/page2',
-            component: Page2
-        },
-        {
-            path: '/login',
-            component: Login
-        },
-        {
-            path: '/registration',
-            component: Registration
-        },
-        {
-            path: '/create',
-            component:CreateNote
-        }
-    ]
-})
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      component: Home,
+      beforeEnter: ifNotLogin,
+    },
+    {
+      path: "/page2",
+      component: Page2,
+      beforeEnter: ifNotLogin,
+    },
+    {
+      path: "/login",
+      component: Login,
+      beforeEnter: ifNotLogin,
+    },
+    {
+      path: "/registration",
+      component: Registration,
+      beforeEnter: ifNotLogin,
+    },
+    {
+      path: "/create",
+      component: CreateNote,
+      beforeEnter: ifLogin,
+    },
+  ],
+});
