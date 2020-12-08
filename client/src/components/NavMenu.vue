22edf086c6abafa4c646176d2445e0a562762709
Vladislav <mitisit89@gmail.com>
2020-12-08 22:08:27 +0300

    Бургер меню

--- client/src/components/NavMenu.vue
+++ client/src/components/NavMenu.vue
@@ -1,53 +1,59 @@
 <template>
-  <nav id="navigation">
-    <div class="nav-menu">
-      <ul class="nav-list">
-        <li class="nav-link">
-          <router-link to="/">Главная</router-link>
-        </li>
-        <li class="nav-link">
-          <router-link to="/page2">Page2</router-link>
-        </li>
-        <template v-if="getIslogin === 'logged'">
-          <li class="nav-link" v-if="checkLogin === true">
-            <router-link to="/create">create</router-link>
-          </li>
-          <li class="nav-link">
-            <a v-on:click="Logout" href="/">Выход</a>
-          </li>
-        </template>
-        <template v-else>
-          <li class="nav-link">
-            <router-link to="/login">Вход</router-link>
-          </li>
-          <li class="nav-link">
-            <router-link to="/registration">Регистрация </router-link>
-          </li>
-        </template>
-      </ul>
+  <header>
+    <div class="nav-toggle">
+      <span  class="material-icons" v-on:click='show = !show'  > menu </span>
     </div>
-    <button id="search" type="submint">
-      <span class="material-icons"> search </span>
-    </button>
-  </nav>
+    <nav id="navigation" v-show="!show">
+      <span v-on:click='show = !show' class="material-icons"> close </span>
+      <div class="nav-menu">
+        <ul class="nav-list">
+          <li class="nav-link">
+            <router-link to="/">Главная</router-link>
+          </li>
+          <li class="nav-link">
+            <router-link to="/page2">Page2</router-link>
+          </li>
+          <template v-if="getLoginStatus === 'logged'">
+            <li class="nav-link">
+              <router-link to="/create">create</router-link>
+            </li>
+            <li class="nav-link">
+              <a v-on:click="logout" href="/">Выход</a>
+            </li>
+          </template>
+          <template v-else>
+            <li class="nav-link">
+              <router-link to="/login">Вход</router-link>
+            </li>
+            <li class="nav-link">
+              <router-link to="/registration">Регистрация </router-link>
+            </li>
+          </template>
+        </ul>
+      </div>
+    </nav>
+  </header>
 </template>
 
 <script>
 import { mapActions, mapGetters } from "vuex";
 export default {
-  computed:mapGetters(["getIslogin"]),
-  methods: {
-    ...mapActions(["logout"]),
-    Logout() {
-      this.logout();
-      this.$router.push("/");
-    },
-   
+  data(){
+    return{
+      show:true
+    }
   },
+  computed: mapGetters(["getLoginStatus"]),
+  methods: mapActions(["logout"]),
+
 };
 </script>
 
 <style>
+header {
+  position: relative;
+  z-index: 1000;
+}
 #navigation {
   display: flex;
   flex-flow: row;
@@ -55,16 +61,22 @@ export default {
   color: white;
   padding: 10px;
   margin: 10px;
-  position: relative;
+  position: fixed;
   align-content: space-between;
   border-radius: 10px;
-  position: fixed;
-  z-index: 1000;
+  width: 300px;
+  top: 0;
+  left: 0;
+  overflow-x: hidden;
 }
 
 .nav-list,
 .auth-list {
-  display: inline-flex;
+  display: flex;
+  flex-direction: column;
+  justify-items: space-around;
+  max-width: 1000px;
+  padding: 1rem;
 }
 
 .nav-link {
