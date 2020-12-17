721d46a0a2de0731816306de728205320b7b1f9c
Vladislav <mitisit89@gmail.com>
2020-12-17 04:26:41 +0300

    component for errors

---
+++ client/src/components/Errors.vue
@@ -0,0 +1,18 @@
+<template>
+    <p>
+        {data.error}
+    </p>
+</template>
+
+<script>
+export default {
+   props:{
+       error:{
+           type:Object,
+           requare:true
+       }
+   },
+    },
+    method:
+}
+</script>
