ca506a15bedb15ba7603fa07a84eac8b65983526
Vladislav <mitisit89@gmail.com>
2021-01-01 13:21:01 +0300

    update regform

--- client/src/components/RegForm.vue
+++ client/src/components/RegForm.vue
@@ -1,46 +1,36 @@
 <template>
-    <form @submit.prevent="onSub">
-        <b-field label="Email"
-            type="is-danger"
-            message="This email is invalid">
-            <b-input type="email"
-                v-model="email"
-                maxlength="30" />
-        </b-field>
-        <b-field label="Username">
-            <b-input
-            v-model="username"
-            />
-        </b-field>
-        <b-field label="Password">
-            <b-input type="password"
-                v-model="password"
-                password-reveal>
-            </b-input>
-            <p>Hell</p>
-        </b-field>
-   <b-button class="button is-primary">Search</b-button>
-   </form>
+  <form class="box" @submit.prevent="onSub">
+    <b-field label="Email">
+      <b-input type="email" v-model="email" maxlength="30" />
+    </b-field>
+    <b-field label="Username">
+      <b-input v-model="username" />
+    </b-field>
+    <b-field label="Password">
+      <b-input type="password" v-model="password" password-reveal> </b-input>
+    </b-field>
+    <b-button class="button is-primary" native-type=submit>Подтвердить</b-button>
+  </form>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-            email:'',
-            username:'',
-            password:''
-        }
+  data() {
+    return {
+      email: "",
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    onSub() {
+      const newUser = {
+        email: this.email,
+        username: this.username,
+        password: this.password,
+      };
+      this.$emit("add-new-user", newUser);
     },
-    methods:{
-        onSub(){
-            const newUser={
-                email:this.email,
-            username:this.username,
-            password:this.password
-            }
-            this.$emit('add-new-user',newUser)
-        }
-    }
-}
+  },
+};
 </script>
