dc698dd3ec26f1e1efe89a53c74511b5a797c290
Vladislav <mitisit89@gmail.com>
2021-03-23 15:54:28 +0300

    use buefy comonents

--- client/src/components/RecipeCard.vue
+++ client/src/components/RecipeCard.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="card">
-    <h2 class="card-title">{{ post.title }}</h2>
-    <div class="content">{{ post.body }}</div>
-    <div class="meta">
-      <time class="datetime">{{ post.time }}</time>
-      <b-tag type="is-info">tags</b-tag>
+    <h1 class="card-title">{{ post.title }}</h1>
+    <div class="card-content">
+      <div class="content">{{ post.body }}</div>
     </div>
-    <button class="rm-btn" v-on:click="$emit('remove-item', post.id)">
-      <span class="material-icons"> delete </span>
-    </button>
+    <div class="card-footer">
+      <b-button size="is-medium" type="is-danger" v-on:click="$emit('remove-item', post.id)">
+        <span class="material-icons"> delete </span>
+      </b-button>
+      <router-link to='/edit'> <b-button size="is-medium"  type="is-warning">
+           <span class="material-icons"> edit </span>
+        </b-button> </router-link>
+    </div>
   </div>
 </template>
 <script>
@@ -40,11 +43,10 @@ export default {
   border: none;
   background: none;
 }
-.material-icons:hover {
-  color: red;
-}
-
 h2 {
   text-decoration-line: underline;
+}
+.card-footer{
+  justify-content: space-around;
 }
 </style>
