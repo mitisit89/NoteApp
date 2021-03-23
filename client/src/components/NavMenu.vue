0d9c7984e0d3ab31b9bf92ddb15be9007e60b3cd
Vladislav <mitisit89@gmail.com>
2021-03-23 15:52:15 +0300

    remove unused styles

--- client/src/components/NavMenu.vue
+++ client/src/components/NavMenu.vue
@@ -31,44 +31,8 @@ header {
   position: relative;
   z-index: 1000;
 }
-#navigation {
-  display: flex;
-  flex-flow: row;
-  background-color: #3d3d3f;
-  color: white;
-  padding: 10px;
-  margin: 10px;
-  position: fixed;
-  align-content: space-between;
-  border-radius: 10px;
-  width: 300px;
-  top: 0;
-  left: 0;
-  overflow-x: hidden;
-}
 
-.nav-list,
-.auth-list {
-  display: flex;
-  justify-items: space-around;
-  max-width: 1000px;
-  padding: 1rem;
-}
 
-.nav-link {
-  flex-flow: row wrap;
-  padding: inherit;
-}
-
-.nav-link a {
-  color: white;
-}
-
-.nav-menu {
-  display: flex;
-  align-content: inherit;
-  align-content: space-between;
-}
 #search {
   margin: 10px 10px 10px;
   overflow: hidden;
