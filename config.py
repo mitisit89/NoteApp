266c41a1036274a10b60f5ea6b126deb468ef6fe
Vladislav <mitisit89@gmail.com>
2021-03-31 17:26:20 +0300

    Working on ref_token

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 
 class Configuration(object):
@@ -7,7 +8,9 @@ class Configuration(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+                              'sqlite:///' + os.path.join(basedir, 'app.db')
     JSON_SORT_KEYS = False
     CORS_METHODS = ['GET', 'POST', 'OPTION', 'DELETE']
     JWT_SECRET_KEY = '12e1esdasd513231'
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
